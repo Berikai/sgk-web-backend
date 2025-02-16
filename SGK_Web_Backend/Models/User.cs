ebc6716cee298faa5118de93494e3a08457d5d86
Berikai <berkayeren2009@gmail.com>
2025-02-16 17:42:36 +0300

    Improve and expand user model

--- SGK_Web_Backend/Models/User.cs
+++ SGK_Web_Backend/Models/User.cs
@@ -1,18 +1,27 @@
-﻿namespace SGK_Web_Backend.Models;
+﻿using SGK_Web_Backend.Enums;
 
+namespace SGK_Web_Backend.Models;
+
+/**
+ * User model
+ *
+ * Required properties: student_id, username, email, password
+ */
 public class User
 {
     public Guid id { get; set; }
-    public string student_id { get; set; }
-    public string username { get; set; }
-    public string email { get; set; } // school email
-    public string password { get; set; } // hashed password with a salt: hash(password + salt)
-    public string name { get; set; }
-    public string surname { get; set; }
-    public string phone_number { get; set; }
-    public string studied_year { get; set; }
-    public string studied_department { get; set; }
-    public string created_at { get; set; }
-    public string ip_created_at { get; set; }
-    public string ip_last { get; set; }
+    public required string student_id { get; set; }
+    public required string username { get; set; }
+    public required string email { get; set; } // School email
+    public required string password { get; set; } // Hashed password with a salt: hash(password + salt) 
+    public UserRole role { get; set; }
+    public string? name { get; set; }
+    public string? surname { get; set; }
+    public string? phone_number { get; set; }
+    public string? studied_year { get; set; }
+    public string? studied_department { get; set; }
+    public string? created_at { get; set; }
+    public string? ip_created_at { get; set; }
+    public string? ip_last { get; set; }
+    public bool verified { get; set; }
 }
